84ef3f3f8d44294eaad6c46eabdd9af5719c974c
Prisca Masereli <priscamasereli@gmail.com>
2025-08-14 12:16:47 +0200

    Feat(components): Add useState to be used

--- src/components/CheckPassword.jsx
+++ src/components/CheckPassword.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CheckPassword = () => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [createdPassword, setCreatedPassword] = useState("");
   return <div></div>;
 };
 
